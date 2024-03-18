23246a0627d54de491aadb069d52bab004f2e7e9
Peter Chapman <pychapman22@gmail.com>
2024-03-18 16:23:44 -0700

    First commit of the tutorial

---
+++ src/data/siteData.ts
@@ -0,0 +1,32 @@
+export const baseData = {
+  title: "Peter Chapman Website",
+  description: "Welcome to my website. Hopefully you'll find this resourceful and enlightening.",
+};
+
+export const socialData = [
+  {
+    title: "dribbble",
+    href: "https://dribbble.com/",
+    icon: "icon--dribbble",
+  },
+  {
+    title: "instagram",
+    href: "https://instagram.com/pychap",
+    icon: "icon--instagram",
+  },
+  {
+    title: "twitter",
+    href: "https://twitter.com/pychap",
+    icon: "icon--twitter",
+  },
+  {
+    title: "youtube",
+    href: "https://www.youtube.com/@PeterChapman-py",
+    icon: "icon--youtube",
+  },
+  {
+    title: "github",
+    href: "https://github.com/pychap",
+    icon: "icon--github",
+  },
+];
